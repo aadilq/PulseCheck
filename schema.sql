2d4b176591f09f71c2a651743bafeb405026e0b7
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2026-05-02 18:44:39 -0400

    create incidents table

--- schema.sql
+++ schema.sql
@@ -10,9 +10,19 @@ CREATE TABLE IF NOT EXISTS checks(
     id                  INTEGER   PRIMARY KEY AUTOINCREMENT,
     url_id              INTEGER   NOT NULL REFERENCES urls(id) ON DELETE CASCADE,
     checked_at          DATETIME  DEFAULT CURRENT_TIMESTAMP,
-    status_code         DATETIME  DEFAULT CURRENT_TIMESTAMP,
+    status_code         INTEGER,
     response_time_ms    INTEGER,
     is_up               INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS incidents(
+    id                  INTEGER   PRIMARY KEY AUTOINCREMENT,
+    url_id              INTEGER   NOT NULL REFERENCES urls(id) ON DELETE CASCADE,
+    started_at          DATETIME  DEFAULT CURRENT_TIMESTAMP,
+    resolved_at         DATETIME,
+    duration_seconds    INTEGER,
+    alert_fired_at      DATETIME,
+    alert_resolved_at   DATETIME
 );
 
 
